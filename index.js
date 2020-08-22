b1c47264035e4587fa002b80bfc8a7ce8d435614
guowenyu <guoruoqing621@126.com>
2020-08-22 18:14:35 +0800

    feat: 梳理setPrototypeOf

--- index.js
+++ index.js
@@ -10,5 +10,17 @@
 // target不可扩展的情况
 // import './GetPrototypeOf/proxy4'
 
-// 拦截哪些操作
-import "./GetPrototypeOf/proxy5"
+// getPrototypeOf拦截哪些操作
+// import "./GetPrototypeOf/proxy5"
+
+// proto需为Object(包含null)
+// import './SetPrototypeOf/proxy1'
+
+// handler 没有setPrototypeOf
+// import './SetPrototypeOf/proxy2'
+
+// setPrototypeOf 返回假值
+// import './SetPrototypeOf/proxy3'
+
+// setPrototypeOf为其他值
+import './SetPrototypeOf/proxy4'
